da6c1514cca87b73af748c84ac49921ceb8e3f2c
mengzizi <2782766569@qq.com>
2020-11-23 20:41:55 +0800

    first

---
+++ mxg-back-admin/src/API/staff.js
@@ -0,0 +1,55 @@
+import request from "../utils/request";
+
+// 获取员工接口
+export function staffinfo(page,pageSize){
+    return request({
+        url:`staff/list/search/${page}/${pageSize}`,
+        method:"POST",
+        data:{
+            page,
+            pageSize
+        }
+    })
+}
+
+// 添加员工接口
+// export function staffAdd(form){
+//     return request({
+//         url:"/member",
+//         method:"POST",
+//         data:{
+//             form,
+//         }
+//     })
+// }
+
+// // 修改员工接口
+// export function staffEdit(form){
+//     return request({
+//         url:"/member/${form.id}",
+//         method:"POST",
+//         data:{
+//             form
+//         }
+//     })
+// }
+
+// // 删除会员接口
+// export function staffDel(form){
+//     return request({
+//         url:"/member/${form.id}",
+//         method:"DELETE",
+//         data:{
+//             form
+//         }
+//     })
+// }
+
+// // 查询单条数据接口
+// export function staffSeaOne(form){
+//     return request({
+//         url:"/member/${form.id}",
+//         method:"POST",
+//         data:form
+//     })
+// }
--- mxg-back-admin/src/views/staff/index.vue
+++ mxg-back-admin/src/views/staff/index.vue
@@ -1,89 +1,171 @@
 <template>
-<div>
-    staff
-</div>
+  <div>
+    <!-- 员工页面 -->
+    <div class="top">员工管理</div>
+    <el-form :inline="true" :model="search" class="demo-form-inline">
+      <el-form-item>
+        <el-input v-model="search.model" placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input v-model="search.user" placeholder="姓名"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">新增</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table :data="staffList" border style="width: 100%">
+      <el-table-column fixed prop="id" label="序号" width="150">
+      </el-table-column>
+      <el-table-column prop="username" label="账号" width="150"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="120">
+      </el-table-column>
+      <el-table-column prop="age" label="年龄" width="120"> </el-table-column>
+      <el-table-column prop="mobile" label="电话" width="200">
+      </el-table-column>
+      <el-table-column prop="salary" label="薪酬" width="150"> </el-table-column>
+      <el-table-column prop="entryDate" label="入职时间" width="180">
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="200">
+        <template slot-scope="scope">
+          <el-button @click="staffDel(scope.row)" type="text" size="small">编辑</el-button>
+          <el-button type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
+import { staffinfo } from "../../API/staff";
 export default {
   // 组件名称
-  name: 'demo',
+  name: "demo",
   // 组件参数 接收来自父组件的数据
   props: [],
   // 局部注册的组件
   components: {},
   // 组件状态值
-  data () {
-   return {}
+  data() {
+    return {
+      staffList: [],
+      search: {
+        name: "",
+        username: "",
+      },
+      // 员工信息
+      list: [],
+      page: 1,
+      pageSize: 20,
+    };
   },
   // 计算属性
   computed: {},
   // 侦听器
   watch: {},
   // 组件方法
-  methods: {},
+  methods: {
+    // 获取渲染数据
+    async getstaff() {
+      const { data: res } = await staffinfo(this.page, this.pageSize);
+      // console.log(res);
+      if (res.flag == true) {
+        this.staffList = res.data.rows;
+      }
+    },
+    // 编辑方法
+    staffDel(){
+      alert("编辑")
+    },
+
+
+    // 删除方法
+
+    // return res = await staffinfo(
+    //   this.page,
+    //   this.pageSize,
+    //   this.search
+    // )
+
+    handleClick() {
+
+    },
+    onSubmit() {},
+  },
   // 以下是生命周期钩子   注：没用到的钩子请自行删除
   /**
-  * 在实例初始化之后，组件属性计算之前，如data属性等
-  */
-  beforeCreate () {
+   * 在实例初始化之后，组件属性计算之前，如data属性等
+   */
+  beforeCreate() {},
+  /**
+   * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
+   */
+  created() {
+    this.getstaff();
   },
   /**
-  * 组件实例创建完成，属性已绑定，但DOM还未生成，$ el属性还不存在
-  */
-  created () {
-  },
+   * 在挂载开始之前被调用：相关的 render 函数首次被调用。
+   */
+  beforeMount() {},
   /**
-  * 在挂载开始之前被调用：相关的 render 函数首次被调用。
-  */
-  beforeMount () {
-  },
+   * el 被新创建的 vm.$ el 替换，并挂载到实例上去之后调用该钩子。
+   * 如果 root 实例挂载了一个文档内元素，当 mounted 被调用时 vm.$ el 也在文档内。
+   */
+  mounted() {},
   /**
-  * el 被新创建的 vm.$ el 替换，并挂载到实例上去之后调用该钩子。
-  * 如果 root 实例挂载了一个文档内元素，当 mounted 被调用时 vm.$ el 也在文档内。
-  */
-  mounted () {
-  },
+   * 数据更新时调用，发生在虚拟 DOM 重新渲染和打补丁之前。
+   * 你可以在这个钩子中进一步地更改状态，这不会触发附加的重渲染过程。
+   */
+  beforeUpdate() {},
   /**
-  * 数据更新时调用，发生在虚拟 DOM 重新渲染和打补丁之前。
-  * 你可以在这个钩子中进一步地更改状态，这不会触发附加的重渲染过程。
-  */
-  beforeUpdate () {
-  },
+   * 由于数据更改导致的虚拟 DOM 重新渲染和打补丁，在这之后会调用该钩子。
+   * 当这个钩子被调用时，组件 DOM 已经更新，所以你现在可以执行依赖于 DOM 的操作。
+   */
+  updated() {},
   /**
-  * 由于数据更改导致的虚拟 DOM 重新渲染和打补丁，在这之后会调用该钩子。
-  * 当这个钩子被调用时，组件 DOM 已经更新，所以你现在可以执行依赖于 DOM 的操作。
-  */
-  updated () {
-  },
+   * keep-alive 组件激活时调用。 仅针对keep-alive 组件有效
+   */
+  activated() {},
   /**
-  * keep-alive 组件激活时调用。 仅针对keep-alive 组件有效
-  */
-  activated () {
-  },
+   * keep-alive 组件停用时调用。 仅针对keep-alive 组件有效
+   */
+  deactivated() {},
   /**
-  * keep-alive 组件停用时调用。 仅针对keep-alive 组件有效
-  */
-  deactivated () {
-  },
+   * 实例销毁之前调用。在这一步，实例仍然完全可用。
+   */
+  beforeDestroy() {},
   /**
-  * 实例销毁之前调用。在这一步，实例仍然完全可用。
-  */
-  beforeDestroy () {
-  },
-  /**
-  * Vue 实例销毁后调用。调用后，Vue 实例指示的所有东西都会解绑定，
-  * 所有的事件监听器会被移除，所有的子实例也会被销毁。
-  */
-  destroyed () {
-  }
-}
+   * Vue 实例销毁后调用。调用后，Vue 实例指示的所有东西都会解绑定，
+   * 所有的事件监听器会被移除，所有的子实例也会被销毁。
+   */
+  destroyed() {},
+};
 </script> 
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <!--使用了scoped属性之后，父组件的style样式将不会渗透到子组件中，-->
 <!--然而子组件的根节点元素会同时被设置了scoped的父css样式和设置了scoped的子css样式影响，-->
 <!--这么设计的目的是父组件可以对子组件根元素进行布局。-->
-<style scoped>
-
+<style scoped lang="scss">
+.top {
+  width: 95%;
+  height: 5vh;
+  border: 1px solid rgb(211, 208, 208);
+  line-height: 5vh;
+  text-align: left;
+}
+.demo-form-inline {
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+}
+th{
+  height: 60px;
+}
 </style>
