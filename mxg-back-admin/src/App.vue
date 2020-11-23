71846d4061550f64e21ee4fea236bd153abede55
yaotianHuMr <925590508@qq.com>
2020-11-24 07:22:11 +0800

    updatestyle

--- mxg-back-admin/src/App.vue
+++ mxg-back-admin/src/App.vue
@@ -15,7 +15,7 @@ html,body{
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   height: 100%;
+  width: 100%;
 }
 </style>
--- mxg-back-admin/src/views/layout/index.vue
+++ mxg-back-admin/src/views/layout/index.vue
@@ -3,22 +3,27 @@
     <el-container>
       <el-header>
         <p class="home_p">
-          <img class="img1" src="http://mengxuegu.com:9999/img/logo.7156be27.png" width="30px" alt />
+          <img
+            class="img1"
+            src="http://mengxuegu.com:9999/img/logo.7156be27.png"
+            width="30px"
+            alt
+          />
           <span>积云后台管理系统</span>
         </p>
         <el-menu
-  class="el-menu-demo"
-  mode="horizontal"
-  background-color="#545c64"
-  text-color="#fff"
-  active-text-color="#ffd04b">
-  <el-submenu index="2">
-    <template slot="title">admin</template>
-    <el-menu-item index="2-1">修改密码</el-menu-item>
-    <el-menu-item index="2-2" @click="logout">退出登录</el-menu-item>
-  
-  </el-submenu>
-</el-menu>
+          class="el-menu-demo"
+          mode="horizontal"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="2">
+            <template slot="title">admin</template>
+            <el-menu-item index="2-1">修改密码</el-menu-item>
+            <el-menu-item index="2-2" @click="logout">退出登录</el-menu-item>
+          </el-submenu>
+        </el-menu>
       </el-header>
       <el-container>
         <el-aside width="200px">
@@ -54,7 +59,7 @@
           </el-menu>
         </el-aside>
         <el-main>
-           <applink v-if="$route.path != '/home'"></applink>
+          <applink v-if="$route.path != '/home'"></applink>
           <!-- 路由占位符 -->
           <router-view></router-view>
         </el-main>
@@ -64,8 +69,8 @@
 </template>
 
 <script>
-import { logout } from '../../API/login'
-import applink from "../applink/applink"
+import { logout } from "../../API/login";
+import applink from "../applink/applink";
 export default {
   // 组件名称
   name: "demo",
@@ -73,7 +78,7 @@ export default {
   props: [],
   // 局部注册的组件
   components: {
-    applink
+    applink,
   },
   // 组件状态值
   data() {
@@ -81,55 +86,34 @@ export default {
       // name:"",
     };
   },
-  created() {
-  },
+  created() {},
   // 计算属性
   computed: {},
   // 侦听器
   watch: {},
   // 组件方法
   methods: {
-  async logout(){
-      const res = await logout()
-      console.log(res)
-      if(res.status == 200){
-        this.$message.success('退出成功')
-        this.$router.push('/login')
+    async logout() {
+      const res = await logout();
+      console.log(res);
+      if (res.status == 200) {
+        this.$message.success("退出成功");
+        this.$router.push("/login");
       }
-  }
-  }
+    },
+  },
 };
 </script> 
 <style lang="scss" scoped>
 .el-header,
 .el-footer {
-  background-color: #b3c0d1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
-
-.el-aside {
-  background-color: #d3dce6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-main {
-  background-color: #e9eef3;
-  color: #333;
-}
-
-.el-container {
-  margin-bottom: 40px;
-}
-.el-header {
+  background-color: #2d3a4b;
+  color: #fff;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  background-color: #2d3a4b;
-  color: #fff;
+  // text-align: center;
+  line-height: 60px;
   > .home_p {
     display: flex;
     align-items: center;
@@ -137,6 +121,34 @@ export default {
       margin-right: 12px;
     }
   }
+}
+
+.el-aside {
+  background-color: #545c64;
+  color: #333;
+  text-align: center;
+  height: 100%;
+  position: absolute;
+  top: 60px;
+  left: 0px;
+  bottom: 0;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  position: absolute;
+  top: 60px;
+  left: 200px;
+  right: 0;
+  bottom: 0;
+  height: 100%;
+}
+
+body>.el-container {
+  position: relative;
+  margin-bottom: 40px;
+  height: 100%;
 }
 > .el-dropdown-link {
   color: #fff;
